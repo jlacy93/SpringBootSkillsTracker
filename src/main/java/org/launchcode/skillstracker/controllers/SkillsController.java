e841cd688555318c07331c4001509f18e428eb37
Josephina Lacy <josephinalacy@gmail.com>
2023-07-12 20:29:12 -0500

    done

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 
 public class SkillsController {
-    @GetMapping(value="") //get stuff for our page
+    @GetMapping(value = "") //get stuff for our page
     @ResponseBody //response
-    public String startSkills(){
-        String html =  "<h1>Skills Tracker</h1>" +
+    public String startSkills() {
+        String html = "<h1>Skills Tracker</h1>" +
                 "<ol>" +
                 "<li>Java</li>" +
                 "<li>Python</li>" +
@@ -21,25 +21,54 @@ public class SkillsController {
         //"<button href='form'>Change it up!</button>";
 
         return html;
-    };
+    }
 
-    @GetMapping(value="form")
+    ;
+
+    @GetMapping(value = "form")
     @ResponseBody
-    public String formSkills(){
-        String html = "<form method = 'post'>" +
+    public String formSkills() {
+        String html = "<form method='post'>" +
                 "Name:<br>" +
                 "<input type = 'text' name = 'name' />" +
+                "<br>My favorite language:<br>" +
+                "<select name = 'firstChoice'>" +
+                "<option value = 'Java'>Java</option>" +
+                "<option value = 'Javascript'>JavaScript</option>" +
+                "<option value = 'Python'>Python</option>" +
+                "</select>" +
+                "<br>My second favorite language:<br>" +
+                "<select name = 'secondChoice'>" +
+                "<option value = 'Java'>Java</option>" +
+                "<option value = 'Javascript'>JavaScript</option>" +
+                "<option value = 'Python'>Python</option>" +
+                "</select>" +
+                "<br>My third favorite language:<br>" +
+                "<select name = 'thirdChoice'>" +
+                "<option value = 'Java'>Java</option>" +
+                "<option value = 'Javascript'>JavaScript</option>" +
+                "<option value = 'Python'>Python</option>" +
+                "</select><br>" +
+                "<input type = 'submit' value = 'Submit' />" +
                 "</form>";
-        return  html;
-    };
-
-    @PostMapping(value="form")
-    @ResponseBody
-    public String namePage(@RequestParam String name){
-        String html = "<h1>" + name + "</h1>" +
-                "<li>" + firstChoice + "</l1>";
         return html;
-    };
+    }
 
+    ;
+
+    @PostMapping(value = "form")
+    @ResponseBody
+    public String namePage(@RequestParam String name, @RequestParam String firstChoice, @RequestParam String secondChoice, @RequestParam String thirdChoice) {
+        String html = "<h1>" + name + "</h1>" +
+                "<ol>" +
+                "<li>" + firstChoice + "</li>" +
+                "<li>" + secondChoice + "</li>" +
+                "<li>" + thirdChoice + "</li>" +
+                "</ol>" +
+                // i added this button for fun lol -hanna
+                "<button onclick=\"window.location.href='http://localhost:8080'\";>reset</button>";
+        return html;
+
+    }
 
 }
